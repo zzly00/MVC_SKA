a8c4a40f12ad39b375648c5966c715a3eb0d6a67
lin.yen55 <lin.yen55@TI03NB181>
2019-08-12 11:07:51 +0800

    first commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var indexViewModel = new IndexViewModel();
+            var random = new Random();
+            for (var i = 0; i < 50; i++)
+            {
+                indexViewModel.Account.Add(new IndexViewModel.AccountModel()
+                {
+                    ID = i + 1,
+                    Category = "支出",
+                    Date = DateTime.Now,
+                    Amount = random.Next(100, 1000)
+                });
+
+            }
+            return View(indexViewModel);
+        }
+
+        public ActionResult _AccountList()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ WebApplication2/ViewModels/IndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication2.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<AccountModel> Account { get; set; } = new List<AccountModel>();
+
+        public class AccountModel
+        {
+            public int ID { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public decimal Amount { get; set; }
+
+        }
+    }
+}
